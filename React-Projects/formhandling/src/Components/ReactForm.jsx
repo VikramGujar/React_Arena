88e00ac764017ad16f6c06c543a706c876d39c76
VikramGujar <vikramgujar300@gmail.com>
2025-02-25 22:08:26 +0530

    Notes of DAY-20 added

---
+++ React-Projects/formhandling/src/Components/ReactForm.jsx
@@ -0,0 +1,19 @@
+
+
+export function ReactForm()
+{
+    return(
+
+        <div className="container-fluid">
+            <h2>User Registration</h2>
+            <form action="">
+
+                <dl>
+                    <dt>Your Name :</dt>
+                    <dd><input type="text" /></dd>
+                </dl>
+
+            </form>
+        </div>
+    );
+}
