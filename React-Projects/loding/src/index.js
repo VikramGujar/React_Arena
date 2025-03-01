3e708e5fd93c18b1389668a2f8bb4a35e2d4c45b
VikramGujar <vikramgujar300@gmail.com>
2025-03-01 22:07:46 +0530

    Creave Cart

--- React-Projects/loding/src/index.js
+++ React-Projects/loding/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import  '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import  '../node_modules/bootstrap/dist/css/bootstrap.css';
 import { LodingImage } from "./Components/LodingImage";
+import CraveCart from "./Components/CraveCart";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LodingImage />
+    <CraveCart />
   </React.StrictMode>
 );
